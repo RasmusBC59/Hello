1cd377c36fe0ddfa4fa10bac98a8ac3f4cf8a90a
RasmusBC59 <rbcmail@gmail.com>
2020-11-02 17:51:51 +0100

    firts commit

---
+++ docs/example_notebooks/index.rst
@@ -0,0 +1,15 @@
+**************************************************
+Examples of using packagename
+**************************************************
+
+Here are examples of using packagename in the format of rendered Jupyter notebooks.
+To experiment with running the example notebooks, please download them directly
+from the packagename git repository.
+
+Basic examples
+--------------
+
+.. toctree::
+    :glob:
+
+    *
---
+++ setup.py
@@ -0,0 +1,35 @@
+"""
+Installs the packagename package
+"""
+
+from setuptools import setup, find_packages
+from pathlib import Path
+
+import versioneer
+
+readme_file_path = Path(__file__).absolute().parent / "README.md"
+
+required_packages = ['opencensus-ext-azure']
+package_data = {"packagename": ["conf/telemetry.ini"] }
+
+
+setup(
+    name="packagename",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    python_requires=">=3.7",
+    install_requires=required_packages,
+    author= "Rasmus Bjerregaard Christensen",
+    author_email="rbcmail@gmail.com",
+    description="package_description",
+    long_description=readme_file_path.open().read(),
+    long_description_content_type="text/markdown",
+    license="",
+    package_data=package_data,
+    packages=find_packages(exclude=["*.tests", "*.tests.*"]),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
